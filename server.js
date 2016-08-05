7ed9775acbeed4ff4853d44c152072e8de41031c
Kedius <kedius@MacBook-Kedius.local>
2016-08-05 22:19:25 +0300

    server side code review

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-const http = require('http'),
+const http = require('http')
+      crypto = require('crypto'),
       url = require('url'),
       moment = require('moment'),
       _ = require('lodash');
 
-const users = [{
+var users = [{
   id: 1,
   username: 'Andrei Markhel',
   email: 'kedius@outlook.com',
-  password: 'qwaszx@1',
+  password: 'a08fd7e3e716e1ac864b633551e8d66bdaff32c2723bde1a181a33daff55ec1d', //qwaszx@1
   date_of_birth: moment.utc('1990-12-10', 'YYYY-MM-DD').format(),
   avatar: 'https://scontent-amt2-1.xx.fbcdn.net/v/t1.0-1/c28.42.348.348/s160x160/267363_191904990872566_1244812_n.jpg?oh=9e3f318eb0b33c3a81a9e8be73b30400&oe=58131950',
-  token: '0KHRitC10YjRjCDQtdGJ0ZEg0Y3RgtC40YUg0LzRj9Cz0LrQuNGFINGE0YDQsNC90YbRg9C30YHQutC40YUg0LHRg9C70L7Qug=='
 }, {
   id: 2,
   username: 'User Example',
   email: 'user@example.com',
-  password: '123456',
-  date_of_birth: moment().format(),
-  avatar: null,
-  token: 'SGVsbG8gd29ybGQh'
+  password: '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', //123456
+  date_of_birth: moment.utc('1970-01-01', 'YYYY-MM-DD').format(),
+  avatar: null
 }];
 
 const Router = function(req, res) {
@@ -80,8 +79,13 @@ const server = http.createServer((req, res) => {
     const router = new Router(req, res);
 
     router.post('/login', (req, res) => {
-      const user = _.find(users, user => user.email === req.body.email && user.password === req.body.password);
+      const user = _.find(users, user => {
+        return user.email === req.body.email
+          && user.password === crypto.createHash('sha256').update(req.body.password).digest('hex');
+      });
+
       if (user) {
+        user.token = (Math.random() * Date.now()).toString();
         res.write(JSON.stringify(_.omit(user, ['password'])));
       } else {
         res.statusCode = 403;
@@ -90,7 +94,10 @@ const server = http.createServer((req, res) => {
     });
 
     router.get('/login', (req, res) => {
-      const user = _.find(users, user => user.token === req.query.accessToken);
+      const user = _.find(users, user => {
+        return user.token && user.token === req.query.accessToken;
+      });
+
       if (user) {
         res.write(JSON.stringify(_.omit(user, ['password'])));
       } else {
@@ -103,6 +110,10 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(7001, 'localhost', () => {
-  console.log(`Server running at http://localhost:7001/`);
+server.listen(process.env.PORT || 7001, 'localhost', () => {
+  console.log(
+    '\x1b[32m%s\x1b[0m \x1b[44m\x1b[37m%s\x1b[0m',
+    'Server started on',
+    `http://localhost:${process.env.PORT || 7001}/`
+  );
 });
