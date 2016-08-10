a0878f670a38c7f377d21e0164eda919b9cee5a0
Kedius <kedius@MacBook-Kedius.local>
2016-08-11 00:26:22 +0300

    lorem ipsum dolor sit amet

--- src/components/no-match.js
+++ src/components/no-match.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 export default class NoMatch extends Component {
   render() {
@@ -6,6 +7,7 @@ export default class NoMatch extends Component {
       <div>
         <h2>404 <small style={{ color: '#777' }}>Page not found!</small></h2>
         <p>Съешь ещё этих мягких французских булок, да выпей же чаю.</p>
+        <Link to="/">Go to the Dashboard</Link>
       </div>
     );
   }
